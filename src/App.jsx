21a7eecd2af12f4e5069ff5ac0c589769439c764
lucastenani <lucas.tenani3@gmail.com>
2023-08-09 16:16:02 -0300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Post from "./Post";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,5 @@
+function Post() {
+  return <h2>Post</h2>;
+}
+
+export default Post;

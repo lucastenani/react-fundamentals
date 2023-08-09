5229a7c4ca28cfd0416bf407e017222ab8bd3714
lucastenani <lucas.tenani3@gmail.com>
2023-08-09 16:17:32 -0300

    Changing components for named export

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Post from "./Post";
+import { Post } from "./Post";
 
-function App() {
+export function App() {
   return (
     <>
       <Post />
@@ -10,5 +10,3 @@ function App() {
     </>
   );
 }
-
-export default App;

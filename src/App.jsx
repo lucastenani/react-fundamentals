2c07c6fe06ce2786a777d812be08e32a0505e4a1
lucastenani <lucas.tenani3@gmail.com>
2023-08-09 17:10:43 -0300

    Using CSS Modules in a new component called Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./styles.css";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Lucas Tenani"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit."

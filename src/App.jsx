c0d04644e5b2e6e5ba528c23268254d6a044907a
lucastenani <lucas.tenani3@gmail.com>
2023-08-14 16:34:40 -0300

    creating the main content style where new components will go

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Lucas Tenani"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-      <Post
-        author="Stephen King"
-        content="Corrupti id natus mollitia repellat officiis aliquid"
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author="Lucas Tenani"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post
+            author="Stephen King"
+            content="Corrupti id natus mollitia repellat officiis aliquid"
+          />
+        </main>
+      </div>
     </>
   );
 }
